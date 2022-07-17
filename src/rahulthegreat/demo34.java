7e3dd3cd4d67d0f9c55eca05536d0cbccabd7779
Akshay <Akshay@DESKTOP-EQU5D95>
2022-07-17 18:40:58 +0530

    [chotu27]:a big project

---
+++ src/rahulthegreat/demo34.java
@@ -0,0 +1,12 @@
+package rahulthegreat;
+
+public class demo34 {
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		System.out.println("hiii");
+		System.out.println("hiii");
+
+		System.out.println("hiii");
+
+	}
+}
